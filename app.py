c6c0929d077f146bf2c1ec51b792aa2a6c58360c
queby <nathanyaqueby21@gmail.com>
2023-05-26 11:09:15 +0200

    try using leafmap

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
+import folium
+import osmnx
+import networkx as nx
+import leafmap.foliumap as leafmap
+from typing import Tuple, List
+from networkx.classes.multidigraph import MultiDiGraph
+
 st.set_page_config(layout="wide",
                 initial_sidebar_state="expanded",
                 page_title="Roadio",
@@ -18,21 +25,114 @@ def get_location_from_address(address: str):
 
     return location.latitude, location.longitude
 
+def get_graph(address_orig: str, address_dest: str):
+
+    MARGIN = 0.1
+
+    # find location by address
+    location_orig = get_location_from_address(address_orig)
+    location_dest = get_location_from_address(address_dest)
+
+    north = max(location_orig[0],location_dest[0]) + MARGIN
+    south = min(location_orig[0],location_dest[0]) - MARGIN
+    west = max(location_orig[1],location_dest[1]) + MARGIN
+    east = min(location_orig[1],location_dest[1]) - MARGIN
+
+    print(f'North: {north}, South: {south}')
+    print(f'West: {west}, East: {east}')
+
+    graph = osmnx.graph.graph_from_bbox(north, south, east, west, network_type='drive', clean_periphery=False)
+
+    return graph, location_orig, location_dest
+
+
+def get_graph_from_mode(address_orig: str, address_dest: str, mode: str, city: str="Brussels", dist: float=1000.):
+
+    assert mode in ['place', 'address']
+
+    # find location by address
+    location_orig = get_location_from_address(address_orig)
+    location_dest = get_location_from_address(address_dest)
+
+    if mode == 'place':
+        graph = osmnx.graph_from_place(city, network_type = 'drive')
+    else:
+        graph = osmnx.graph.graph_from_address(address_orig, dist=dist, dist_type='bbox', network_type = 'drive')
+
+    return graph, location_orig, location_dest
+
+def find_shortest_path(graph: MultiDiGraph, location_orig: Tuple[float], location_dest: Tuple[float], optimizer: str) -> List[int]:
+    # find the nearest node to the departure and arrival location
+    node_orig = osmnx.get_nearest_node(graph, location_orig)
+    node_dest = osmnx.get_nearest_node(graph, location_dest)
+
+    route = nx.shortest_path(graph, node_orig, node_dest, weight=optimizer.lower())
+    return route
+
+def clear_text():
+    st.session_state["go_from"] = ""
+    st.session_state["go_to"] = ""
 ############ sidebar
 
-st.sidebar.header("Insert your home address")
-address = st.sidebar.text_input("Address", "Am Kreuzhof 2, Regensburg")
+with st.sidebar:
 
-# get location from address
-lat, lon = get_location_from_address(address)
+    with st.form(key='generate_route'):
+        st.header("Generate your route")
 
-st.sidebar.header("Insert your work address")
-work_address = st.sidebar.text_input("Address", "Friedrich-Viehbacher-Allee 5, Regensburg")
+        basemap = st.selectbox("Choose basemap", ['Satellite', 'Roadmap', 'Terrain', 'Hybrid', 'OpenStreetMap'])
+        if basemap in ['Satellite', 'Roadmap', 'Terrain', 'Hybrid', 'OpenStreetMap'][:-1]:
+            basemap=basemap.upper()
 
-# get location from address
-work_lat, work_lon = get_location_from_address(work_address)
+        st.subheader("Choose your mode of transport")
+        mode = st.selectbox("Mode", ["Car", "Bike", "Motorcycle", "E-Scooter", "Other"])
+        
+        st.subheader("Choose your optimizer")
+        optimizer = st.selectbox("Optimizer", ["Distance", "Travel time"])
+
+        st.subheader("Choose your departure location")
+        address_orig = st.text_input("Address", "Am Kreuzhof 2, Regensburg")
+        
+        st.subheader("Choose your destination location")
+        address_dest = st.text_input("Address", "Friedrich-Viehbacher-Allee 5, Regensburg")
+        submit_button = st.form_submit_button(label='Generate route')
 
 ############ main
+
+lat, lon = get_location_from_address(address=address_orig)
+
+m = leafmap.Map(center=(lat, lon), zoom=16)
+
+m.add_basemap(basemap)
+
+if submit_button and address_orig and address_dest:
+
+    # Find the route
+    graph, location_orig, location_dest = get_graph(address_orig, address_dest)
+
+    # Search information 
+    st.markdown(f'**From**: {address_orig}')
+    st.markdown(f'**To**: {address_dest}')
+    st.write(graph)
+
+    # re-center
+    leafmap.Map(center=location_orig, zoom=16)
+
+    # find the nearest node to the start location
+    m.add_marker(location=list(location_orig), icon=folium.Icon(color='red', icon='suitcase', prefix='fa'))
+    m.add_marker(location=list(location_dest), icon=folium.Icon(color='green', icon='street-view', prefix='fa'))
+
+    # find the shortest path
+    route = find_shortest_path(graph, location_orig, location_dest, optimizer)
+
+    osmnx.plot_route_folium(graph, route, m)
+
+else:
+
+    m.add_marker(location=(lat, lon), popup=f"lat, lon: {lat}, {lon}", icon=folium.Icon(color='green', icon='eye', prefix='fa'))
+    st.write(f"Lat, Lon: {lat}, {lon}")
+
+
+m.to_streamlit()
 
 mapbox_access_token = st.secrets["mapbox_access_token"]
 
