b99d47e894010e3d6c842fe80d0219c44906f0a9
queby <nathanyaqueby21@gmail.com>
2023-05-26 15:46:06 +0200

    add pages

--- app.py
+++ app.py
@@ -13,8 +13,12 @@ from networkx.classes.multidigraph import MultiDiGraph
 st.set_page_config(layout="wide",
                 initial_sidebar_state="expanded",
                 page_title="Roadio",
-                page_icon="🚗"
-                )
+                page_icon="🚗",
+                menu_items={
+                    'Get Help': 'https://www.github.com/nathanyaqueby/roadio/',
+                    'Report a bug': "https://www.github.com/nathanyaqueby/roadio/issues",
+                    'About': "https://www.github.com/nathanyaqueby/roadio/"
+                })
 
 ############
 def get_location_from_address(address: str):
---
+++ pages/1_💺_Carpooling.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(layout="wide",
+                initial_sidebar_state="expanded",
+                page_title="Roadio",
+                page_icon="🚗",
+                menu_items={
+                    'Get Help': 'https://www.github.com/nathanyaqueby/roadio/',
+                    'Report a bug': "https://www.github.com/nathanyaqueby/roadio/issues",
+                    'About': "https://www.github.com/nathanyaqueby/roadio/"
+                })
+
+st.title("Carpooling")
